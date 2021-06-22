2245f421b0625c0f75c3fe330c3bd5f53092ef22
yannbanvi <yannbanvi@users.noreply.github.com>
2021-06-22 06:38:25 +0100

    Creation des migrations & ajout des relations au niveau des tables

---
+++ database/migrations/2021_06_22_045506_create_clients_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string("nom");
+            $table->string("prenom");
+            $table->char("sexe");
+            $table->date("dateNaissance");
+            $table->string("lieuNaissance");
+            $table->string("nationalite");
+            $table->string("ville");
+            $table->string("pays");
+            $table->string("adresse");
+            $table->string("telephone1");
+            $table->string("telephone2")->nullable();
+            $table->string("pieceIdentite");
+            $table->string("noPieceIdentite");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
